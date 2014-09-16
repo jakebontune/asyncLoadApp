f44914c97c0e6d994fd16ae221b4de0a6ecee329
Kolade Joseph Ayo-Vaughan <joey.ayov@gmail.com>
2014-09-16 17:09:54 -0400

    Revert "scale images with HTML5 canvas"
    This reverts commit e1d1c00714e0b5fc77ba29c286c67e3ccba7c3c2.

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	console.log("device is ready");
 	for (var i = 0; i <= 100; i++) {
-		$(".images-wrap").append("<div class='image'><canvas class='canvas img" + i + "' width='50px' height='50px'></canvas></div>");
+		$(".images-wrap").append("<div class='image'><img class='img" + i + "' src='http://dummyimage.com/50x50/000/fff.jpg&text=Loading' alt='' /></div>");
 	};
 
 	$(".reload").on("click", reloadPage);
@@ -14,26 +14,11 @@ function onDeviceReady() {
 
 function loadImages() {
 	console.log("Loading images");
-
 	$.getJSON("http://54.214.19.19/include/mobile/getDiscover.php?category=curly", function(data) {
 		// console.log(data);
 		$.each(data, function (key, val) {
-			console.log(key + ": " + val.pic_name);
-			// $(".img" + key).attr("src", val.pic_name);
-			var canvas = document.getElementsByClassName("img" + key)[0];
-			console.log(canvas);
-			var ctx = canvas.getContext("2d");
-			var x = 0;
-			var y = 0;
-			var width = 50;
-			var height = 50;
-			var img = new Image();
-			img.onload = function () {
-				ctx.drawImage(img, x, y, width, height);
-			}
-			img.src = val.pic_name;
-			// canvas.css("border-color", "red");
-			console.log("done!!!");
+			console.log(key + ": " + val)
+			$(".img" + key).attr("src", val.pic_name);
 		});
 	});
 }
@@ -42,7 +27,7 @@ function isNearBottom() {
 	$(window).scroll(function() {
 	   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
 	      	for (var i = 0; i <= 50; i++) {
-				$(".images-wrap").append("<div class='image'><canvas class='canvas img" + i + "' width='50px' height='50px'></canvas></div>");
+				$(".images-wrap").append("<div class='image'><img class='img" + i + "' src='http://dummyimage.com/50x50/000/fff.jpg&text=Loading' alt='' /></div>");
 			};
 			loadImages();
 	   }
--- www/js/main.js
+++ www/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	console.log("device is ready");
 	for (var i = 0; i <= 100; i++) {
-		$(".images-wrap").append("<div class='image'><canvas class='canvas img" + i + "' width='50px' height='50px'></canvas></div>");
+		$(".images-wrap").append("<div class='image'><canvas class='img" + i + "' width='50px'></canvas></div>");
 	};
 
 	$(".reload").on("click", reloadPage);
@@ -14,26 +14,34 @@ function onDeviceReady() {
 
 function loadImages() {
 	console.log("Loading images");
+	var canvas = $(".canvas");
+	var ctx = canvas.getContext("2d");
 
+	img = new Image();
+	img.onload = function () {
+
+	    canvas.height = canvas.width * (img.height / img.width);
+
+	    /// step 1
+	    var oc = document.createElement('canvas'),
+	        octx = oc.getContext('2d');
+
+	    oc.width = img.width * 0.5;
+	    oc.height = img.height * 0.5;
+	    octx.drawImage(img, 0, 0, oc.width, oc.height);
+
+	    /// step 2
+	    octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
+
+	    ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
+	    0, 0, canvas.width, canvas.height);
+	}
+img.src = "http://i.imgur.com/SHo6Fub.jpg";
 	$.getJSON("http://54.214.19.19/include/mobile/getDiscover.php?category=curly", function(data) {
 		// console.log(data);
 		$.each(data, function (key, val) {
-			console.log(key + ": " + val.pic_name);
-			// $(".img" + key).attr("src", val.pic_name);
-			var canvas = document.getElementsByClassName("img" + key)[0];
-			console.log(canvas);
-			var ctx = canvas.getContext("2d");
-			var x = 0;
-			var y = 0;
-			var width = 50;
-			var height = 50;
-			var img = new Image();
-			img.onload = function () {
-				ctx.drawImage(img, x, y, width, height);
-			}
-			img.src = val.pic_name;
-			// canvas.css("border-color", "red");
-			console.log("done!!!");
+			console.log(key + ": " + val)
+			$(".img" + key).attr("src", val.pic_name);
 		});
 	});
 }
@@ -42,7 +50,7 @@ function isNearBottom() {
 	$(window).scroll(function() {
 	   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
 	      	for (var i = 0; i <= 50; i++) {
-				$(".images-wrap").append("<div class='image'><canvas class='canvas img" + i + "' width='50px' height='50px'></canvas></div>");
+				$(".images-wrap").append("<div class='image'><img class='img" + i + "' src='http://dummyimage.com/50x50/000/fff.jpg&text=Loading' alt='' /></div>");
 			};
 			loadImages();
 	   }
